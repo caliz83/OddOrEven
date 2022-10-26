7361903bb8f6aaaf8dbbb43c0810bea6fd6c97df
caliz83 <lizg1308@gmail.com>
2022-10-25 19:51:39 -0700

    Add code

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿bool letsPlay = true;
+
+while(letsPlay == true)
+{
+int number;
+
+Console.WriteLine("Please enter a number.");
+number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0){
+    Console.WriteLine(number + " is an even number.");
+}
+else{
+    Console.WriteLine(number + " is an odd number.");
+}
+bool question = true;
+while(question == true)
+{
+Console.WriteLine("Do you want to play again? Y/N");
+string playAgain = Console.ReadLine().ToUpper();
+
+if(playAgain == "Y")
+{
+    letsPlay = true;
+    question = false;
+}
+else if(playAgain == "N")
+{
+    letsPlay = false;
+    question = false;
+    Console.WriteLine("Goodbye.");
+}
+else
+{
+    Console.WriteLine("Please enter a valid option: Y or N");
+    question = true;
+}
+}
+}
